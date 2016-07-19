3e2d219666301dcba6308d13bb6ca05c6974e111
Curtis Rueden <ctrueden@wisc.edu>
2016-07-19 12:11:36 -0500

    BaseTrackMatePlugIn: add SJC Context accessor
    This will be handy for injecting the context downstream,
    instead of needing to create a new one ever.

--- src/main/java/fiji/plugin/trackmate/BaseTrackMatePlugIn.java
+++ src/main/java/fiji/plugin/trackmate/BaseTrackMatePlugIn.java
@@ -1,7 +1,14 @@
 package fiji.plugin.trackmate;
 
+import org.scijava.Context;
+
+import ij.IJ;
 import ij.plugin.PlugIn;
 
 public interface BaseTrackMatePlugIn extends PlugIn {
-	// NB: Marker interface.
+
+	default Context context() {
+		return (Context) IJ.runPlugIn("org.scijava.Context", "");
+	}
+
 }
