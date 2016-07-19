1fa21c9fc044c8aa5fb415f54e990952c431cfe7
Curtis Rueden <ctrueden@wisc.edu>
2016-07-19 13:13:41 -0500

    Add a marker interface for TrackMate services
    This will make it easy to create SciJava contexts with all TrackMate
    services available, as well as easy to browse (in an IDE) all the
    services which TrackMate provides.

---
+++ src/main/java/fiji/plugin/trackmate/TrackMateService.java
@@ -0,0 +1,12 @@
+package fiji.plugin.trackmate;
+
+import net.imagej.ImageJService;
+
+/**
+ * Interface for all TrackMate-related services.
+ * 
+ * @author Curtis Rueden
+ */
+public interface TrackMateService extends ImageJService {
+	// NB: Marker interface.
+}
